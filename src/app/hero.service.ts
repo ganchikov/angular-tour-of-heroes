3c4d7a4d01cf12c9e27a90ab473a7961a3272cf9
ganchikovm <ganchikovm@github.com>
2018-01-21 19:51:56 +0300

    search added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -71,5 +71,17 @@ export class HeroService {
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
     );
-}
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
 }
